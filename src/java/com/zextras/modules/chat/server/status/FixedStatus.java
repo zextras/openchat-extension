5da710fb614d61f96dd931fa1f7e3412f96c35b8
Davide Baldo <db@zextras.com>
2018-02-23 15:10:31 +0100

    Fixed typo Unsuscribed=>Unsubscribed

--- src/java/com/zextras/modules/chat/server/status/FixedStatus.java
+++ src/java/com/zextras/modules/chat/server/status/FixedStatus.java
@@ -35,7 +35,7 @@ public class FixedStatus extends StatusAdapter implements Status
   public static final FixedStatus NeedResponse = new FixedStatus(StatusType.NEED_RESPONSE);
   public static final FixedStatus Invited      = new FixedStatus(StatusType.INVITED);
   public static final FixedStatus Unreachable  = new FixedStatus(StatusType.UNREACHABLE);
-  public static final FixedStatus Unsuscribed  = new FixedStatus(StatusType.UNSUBSCRIBED);
+  public static final FixedStatus Unsubscribed = new FixedStatus(StatusType.UNSUBSCRIBED);
 
   public FixedStatus(StatusType type)
   {

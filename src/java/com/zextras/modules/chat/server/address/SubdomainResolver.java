138a508e27771dec914d521fea031c84f30b54c9
Dino Puller <dp@zextras.com>
2018-12-04 16:19:53 +0100

    [ZX-5484] simplified SubdomainResolver for openchat

--- src/java/com/zextras/modules/chat/server/address/SubdomainResolver.java
+++ src/java/com/zextras/modules/chat/server/address/SubdomainResolver.java
@@ -1,31 +1,15 @@
 package com.zextras.modules.chat.server.address;
 
 import com.zextras.modules.chat.server.events.TargetType;
-import org.openzal.zal.Provisioning;
-
-import javax.inject.Inject;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
 
 /**
  * Conversion from and to chat subdomain.
  *
  * TODO:
- *  - avoid getAccountByName()
  *  - introduce configuration option to change the subdomain
  */
 public class SubdomainResolver
 {
-  private final Provisioning mProvisioning;
-
-  @Inject
-  public SubdomainResolver(
-    Provisioning provisioning
-  )
-  {
-    mProvisioning = provisioning;
-  }
-
   public String getSubdomainFor(String domain)
   {
     return "chat."+domain;
@@ -33,52 +17,21 @@ public class SubdomainResolver
 
   public String removeSubdomainFrom(TargetType eventType, String room)
   {
-    if (eventType != TargetType.Chat)
-    {
-      return room.replace("@chat.", "@");
-    }
     return room;
   }
 
   public String removeSubdomainFrom(String room)
   {
-    if (!isValidAccountName(room))
-    {
-      return room.replace("@chat.", "@");
-    }
     return room;
+  }
+
+  public SpecificAddress toRoomAddress(TargetType eventType, String room)
+  {
+    return new SpecificAddress(room);
   }
 
   public SpecificAddress toRoomAddress(String room)
   {
-    if (!isValidAccountName(room))
-    {
-      String[] strings = room.split("@");
-      if (strings.length >= 2)
-      {
-        return new SpecificAddress(strings[0] + "@chat." + strings[1]);
-      }
-    }
     return new SpecificAddress(room);
-  }
-
-  public boolean isValidAccountName(String name)
-  {
-    return isValidEmail(name) && mProvisioning.getAccountByName(name) != null;
-  }
-
-  public boolean isValidEmail(String email)
-  {
-    boolean result = true;
-    try
-    {
-      InternetAddress emailAddr = new InternetAddress(email);
-      emailAddr.validate();
-    }
-    catch (AddressException ex)
-    {
-      result = false;
-    }
-    return result;
   }
 }

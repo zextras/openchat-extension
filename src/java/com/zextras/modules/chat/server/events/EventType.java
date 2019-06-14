57ed966fc75046857d6f58411b9a62fef6d4d0d3
Matteo Battilana <matteo.battilana@zextras.com>
2019-06-14 12:57:44 +0200

    [TEAMS-415] - Added ConversationDeleted event type

--- src/java/com/zextras/modules/chat/server/events/EventType.java
+++ src/java/com/zextras/modules/chat/server/events/EventType.java
@@ -11,7 +11,8 @@ public enum EventType
   ConversationTopicChanged,
   ConversationPictureChanged,
   MeetingStarted,
-  MeetingEnded
+  MeetingEnded,
+  ConversationDeleted
   ;
 
   public static EventType fromShort(short typeString)
@@ -28,6 +29,7 @@ public enum EventType
       case 7: return ConversationPictureChanged;
       case 8: return MeetingStarted;
       case 9: return MeetingEnded;
+      case 10: return ConversationDeleted;
       default: throw new RuntimeException("invalid: "+typeString);
     }
   }
@@ -46,6 +48,7 @@ public enum EventType
       case ConversationPictureChanged: return 7;
       case MeetingStarted: return 8;
       case MeetingEnded: return 9;
+      case ConversationDeleted: return 10;
       default: throw new RuntimeException("invalid: "+type);
     }
   }
